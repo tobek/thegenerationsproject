ba0d9b7e7ff1bc0965c473386c29c5b9b7eb0d75
Toby Fox <toby@digg.com>
2016-03-18 09:04:33 -0400

    First pass at single post page

--- web/app/themes/tgp-theme/templates/content-single.php
+++ web/app/themes/tgp-theme/templates/content-single.php
@@ -1,15 +1,26 @@
+<?php
+    $two_col = false;
+
+?>
+
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
-  </article>
+    <article <?php post_class(); ?>>
+        <header class="page-header container">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <?php get_template_part('templates/entry-meta'); ?>
+        </header>
+
+        <div class="container">
+            <div class="row">
+                <div class="<?= $two_col ? 'col-ms-12 col-sm-7' : 'col-sm-9 col-lg-7' ?> post-content-wrapper">
+                    <div class="post-content">
+                        <div class="post-content-inner">
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php // comments_template('/templates/comments.php'); ?>
+    </article>
 <?php endwhile; ?>
