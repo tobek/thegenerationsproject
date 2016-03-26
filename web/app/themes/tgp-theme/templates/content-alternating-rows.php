8ca48467a87c408dbf16311398a52188b35d1546
Toby Fox <toby@digg.com>
2016-03-26 03:01:24 -0400

    Storytellers page

--- web/app/themes/tgp-theme/templates/content-alternating-rows.php
+++ web/app/themes/tgp-theme/templates/content-alternating-rows.php
@@ -4,16 +4,18 @@
         $event_date = date('M d, Y', $date);
     }
 
+    global $dont_link_posts;
+    $link_posts = ! isset($dont_link_posts) || ! $dont_link_posts;
 ?>
 
 <article class="alternating-rows tgp-post js-post">
-    <a class="image" href="<?= get_the_permalink() ?>">
+    <a class="image" <?php if ($link_posts) { ?>href="<?= get_the_permalink() ?>"<?php } ?>>
         <?= get_the_post_thumbnail(null, 'thumbnail') ?>
     </a>
 
 
     <header>
-        <a class="title" href="<?= get_the_permalink() ?>">
+        <a class="title" <?php if ($link_posts) { ?>href="<?= get_the_permalink() ?>"<?php } ?>>
             <h3><?= get_the_title() ?></h3>
         </a>
         <?php if (get_post_meta($post->ID, 'event_date', true)) { ?>
--- web/app/themes/tgp-theme/templates/content-page.php
+++ web/app/themes/tgp-theme/templates/content-page.php
@@ -1,11 +1,24 @@
 <div class="container">
 <?php
-    $sub_pages_query = new WP_Query([
-        'post_type' => 'page',
-        'post_parent' => $post->ID,
-        'orderby' => 'menu_order',
-        'order' => 'ASC',
-    ]);
+    if ($post->post_name === 'storytellers') {
+        $sub_pages_query = new WP_Query([
+            'post_type' => 'tgp_person',
+            'tgp_role' => 'storyteller',
+            'orderby' => 'menu_order',
+            'order' => 'ASC',
+        ]);
+
+        global $dont_link_posts;
+        $dont_link_posts = true;
+    }
+    else {
+        $sub_pages_query = new WP_Query([
+            'post_type' => 'page',
+            'post_parent' => $post->ID,
+            'orderby' => 'menu_order',
+            'order' => 'ASC',
+        ]);
+    }
 
     $index = 0;
 
